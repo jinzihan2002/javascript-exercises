1c152b928a16bec9f16ae6143c4dcf54b1b8c252
Jin Zihan <88239903+jinzihan2002@users.noreply.github.com>
2024-07-10 13:48:32 +0800

    Add attempt 1 for 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (num1 <= 0 || num2 <= 0 || !Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return "ERROR";
+    }
+    let smaller, larger;
+    if (num1 > num2) {
+        larger = num1;
+        smaller = num2;
+    } else {
+        larger = num2;
+        smaller = num1;
+    }
+    num1 = smaller;
+    while (smaller < larger) {
+        num1 += ++smaller;
+    }
+    return num1;
 };
 
 // Do not edit below this line
