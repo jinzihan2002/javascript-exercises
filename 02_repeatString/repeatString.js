097f6aa3185aea76779dce2b2d33054f160bfc05
Jin Zihan <88239903+jinzihan2002@users.noreply.github.com>
2024-07-10 12:30:34 +0800

    Add attempt 1 for 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(inputStr, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    let result = "";
+    for (let i = 0; i < num; ++i) {
+        result += inputStr;
+    }
+    return result;
 };
 
 // Do not edit below this line
