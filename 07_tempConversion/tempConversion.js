e542c469d6372ba6665167946952d61523e044d3
Jin Zihan <88239903+jinzihan2002@users.noreply.github.com>
2024-07-10 14:10:17 +0800

    Add attempt 1 for 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const roundToOne = function(num) {
+  return Math.round(num * 10) / 10;
+}
+
+const convertToCelsius = function(tempF) {
+  return roundToOne((tempF - 32) * 5 / 9);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  return roundToOne(tempC * 9 / 5 + 32);
 };
 
 // Do not edit below this line
