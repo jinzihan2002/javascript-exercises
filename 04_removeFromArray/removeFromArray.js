28388e2da66f246d678c639d87b4e3a70a1bba8b
Jin Zihan <88239903+jinzihan2002@users.noreply.github.com>
2024-07-10 13:33:23 +0800

    Add attempt 1 for 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArr, ...elements) {
+    for (const element of elements) {
+        let index = inputArr.indexOf(element)
+        while (index != -1) {
+            inputArr.splice(index, 1);
+            index = inputArr.indexOf(element)
+        }
+    }
+    return inputArr;
 };
 
 // Do not edit below this line
