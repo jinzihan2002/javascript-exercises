bc1bf2749e0c0e761b1bdaeb6b1ff99acb1d781a
Jin Zihan <88239903+jinzihan2002@users.noreply.github.com>
2024-07-19 14:16:16 +0800

    Add attempt 1 for 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = parseInt(num);
+    if (num < 0) {
+        return "OOPS";
+    }
+    if (num === 0) {
+        return 0;
+    }
+    let a = 0, b = 1, curr;
+    for (let i = 1; i < num; ++i) {
+        curr = a + b;
+        a = b;
+        b = curr;
+    }
+    return b;
 };
 
 // Do not edit below this line
