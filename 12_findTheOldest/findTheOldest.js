649796ad4dee4cca77014a2cb9d994af896f42c1
Jin Zihan <88239903+jinzihan2002@users.noreply.github.com>
2024-07-19 15:13:58 +0800

    Add attempt 1 for 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
+const currYear = new Date().getFullYear();
 
+const findTheOldest = function(people) {
+    people.sort(compareAge);
+    return people[0];
 };
+
+function getAge(person) {
+    if (!("yearOfDeath" in person)) {
+        return currYear - person["yearOfBirth"];
+    }
+    return person["yearOfDeath"] - person["yearOfBirth"];
+}
+
+function compareAge(a, b) {
+    if (getAge(a) > getAge(b)) return -1;
+    if (getAge(a) == getAge(b)) return 0;
+    if (getAge(a) < getAge(b)) return 1;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
