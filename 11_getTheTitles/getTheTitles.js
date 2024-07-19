1832d71e319c147bf5402ebf361bc5b88c71bd1e
Jin Zihan <88239903+jinzihan2002@users.noreply.github.com>
2024-07-19 14:40:21 +0800

    Add attempt 1 for 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,15 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const titles = [];
+    books.forEach(book => {
+        titles.push(book.title);        
+    });
+    return titles;
 };
+
+// Wow
+// const getTheTitles = function (array) {
+//     return array.map((book) => book.title);
+// };
 
 // Do not edit below this line
 module.exports = getTheTitles;
