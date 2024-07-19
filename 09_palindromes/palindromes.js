36d251008042682435600a56760e09e35e2183b5
Jin Zihan <88239903+jinzihan2002@users.noreply.github.com>
2024-07-19 13:49:55 +0800

    Add attempt 1 for 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    input = input.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    let left = 0;
+    let right = input.length - 1;
+    while (left < right) {
+        if (input[left] !== input[right]) {
+            return false;
+        }
+        ++left;
+        --right;
+    }
+    return true;
 };
 
 // Do not edit below this line
